0002daa30f121f7828c79b9fd9ad8ae4b53e2dbd
Belousoveu <belousov.eu@gmail.com>
2024-07-05 10:14:20 +0300

    Complete tasks

---
+++ .venv/datas.py
@@ -0,0 +1,17 @@
+## 1st program
+print(9 ** 0.5 * 5)
+
+## 2nd program
+print(9.99 > 9.98 and 1000 != 1001)
+
+## 3rd program
+# 1st method
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+# 2nd method
+print(int(1234 % 1000 / 10) + int(5678 % 1000 / 10))
+
+## 4th program
+# 1st method
+print(13.42 * 100 // 100 == 42.13 * 100 % 100 or 42.13 * 100 // 100 == 13.42 * 100 % 100)
+#  2nd method
+print(int(13.42) == int(42.13 % 1 * 100) or int(42.13) == int(13.42 % 1 * 100))
